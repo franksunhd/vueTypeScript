652e0c773e6b7e040f022c1cb918b1a5ea5774f8
fransun <sun_si_yan@163.com>
2024-05-14 21:02:54 +0800

    提交

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+        <h1>boolean:{{bool.toString()}}</h1>
+        <h1>string:{{str.toString()}}</h1>
+        <h1>number:{{num.toString()}}</h1>
+        <button @click="clickBtn">点击</button>
     </div>
 </template>
 
 <script lang="ts">
-	import {defineComponent} from "vue";
-	import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+	import {defineComponent, ref} from "vue";
 
 	export default defineComponent({
 		name: "HomeView",
-		components: {
-			HelloWorld
+		setup() {
+			let bool = ref<boolean>(true);
+			let str = ref("123");
+			let num = ref<number>(12);
+
+			function clickBtn() {
+				num.value++;
+			}
+
+			return {bool, str, num, clickBtn};
 		}
+
 	});
 </script>
